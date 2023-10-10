ca734b99d51ac76d68b4b6e3511ad69c516b09e8
Henrique Pinho <1230191@isep.ipp.pt>
2023-10-10 22:11:35 +0100

    script to creat diagram's PNG

---
+++ scripts/genPUML/genPUMListPopular.js
@@ -0,0 +1,24 @@
+const { execSync } = require('child_process');
+const path = require('path');
+var plantuml = require('node-plantuml');
+var fs = require('fs');
+
+// Define the JSDoc generation process
+function genPUMListPopular() {
+    // Set the working directory to the root of the project
+    const projectRoot = path.resolve(__dirname, '../../');
+    process.chdir(projectRoot);
+
+    try {
+      // The flag --dry-run is used to simulate the installation of the dependencies
+      // The object { stdio: 'inherit' } is used to display the output of the command in the console
+      execSync('npm install node-plantuml', { stdio: 'inherit' });
+
+      var gen = plantuml.generate("docs/plantUML/listPopular.puml", {format: 'svg'});
+      gen.out.pipe(fs.createWriteStream("images/list-popular.svg"));
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+}
+genPUMListPopular() 
